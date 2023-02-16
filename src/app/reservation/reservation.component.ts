cfb6da59e7f759ba8ee62f9c014a0cbdf1222853
Pawel Lukaszewicz <pawel.lukaszewicz@gft.com>
2023-02-16 21:31:51 +0100

    Reservation form

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit{
+  
+  private date: string;
+  private duration: string;
+  private numberOfGuest: string;
+  private name: string;
+  private email: string;
+  private phoneNumber: string;
+  private message: string;
+
+  constructor() {
+    this.date = '';
+    this.duration = '';
+    this.numberOfGuest = '';
+    this.name = '';
+    this.email = '';
+    this.phoneNumber = '';
+    this.message = '';
+  }
+
+  ngOnInit(): void {
+    this.date = '';
+    this.duration = '';
+    this.numberOfGuest = '';
+    this.name = '';
+    this.email = '';
+    this.phoneNumber = '';
+    this.message = '';
+  }
+
+  createReservation() {
+    console.log(this.message)
+  }
 
 }
