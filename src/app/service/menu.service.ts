fbf6c88b83897318a21c74d24df14fb2d8a5956f
shaqu <pawel.lukaszewicz99@gmail.com>
2023-02-17 23:50:26 +0100

    MenuService

--- src/app/service/menu.service.ts
+++ src/app/service/menu.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../environment";
+import {Observable} from "rxjs";
+import {Location} from "../dtos/Location";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  apiUrl = environment.baseUrl;
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<any> {
+    return this.http.get<Location[]>(`${this.apiUrl}menu/categories/1`);
+  }
+
+  getMenuPositionByCategory(categoryId: number): Observable<any> {
+    return this.http.get<Location[]>(`${this.apiUrl}menu/category/${categoryId}`);
+  }
+
+  getMenuPositionPicture(menuPositionId: number): Observable<any> {
+    return this.http.get<Location[]>(`${this.apiUrl}menu/${menuPositionId}/picture`);
+  }
 }
